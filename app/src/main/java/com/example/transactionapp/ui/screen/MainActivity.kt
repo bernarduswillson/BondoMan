4afbe1708a84e12054a0d492bf8ae3d807daedc9
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-15 17:49:07 +0700

    task: add navbar unstyled

--- app/src/main/java/com/example/transactionapp/ui/screen/MainActivity.kt
+++ app/src/main/java/com/example/transactionapp/ui/screen/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.transactionapp.ui.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
 import com.example.transactionapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Enable action bar.
+        setSupportActionBar(findViewById(R.id.actionBar))
     }
 }
