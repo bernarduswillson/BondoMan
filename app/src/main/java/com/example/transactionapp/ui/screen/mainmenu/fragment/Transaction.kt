4ef1c3848b69643485400ba3623016db0bf811a5
Fahrian Afdholi <89321009+fchrgrib@users.noreply.github.com>
2024-03-17 01:04:39 +0700

    feat: add header transaction

--- app/src/main/java/com/example/transactionapp/ui/screen/mainmenu/fragment/Transaction.kt
+++ app/src/main/java/com/example/transactionapp/ui/screen/mainmenu/fragment/Transaction.kt
@@ -1,18 +1,14 @@
 package com.example.transactionapp.ui.screen.mainmenu.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.example.transactionapp.databinding.FragmentTransactionBinding
-import com.example.transactionapp.domain.db.model.Transaction
 import com.example.transactionapp.ui.viewmodel.transaction.TransactionViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.Date
 
 @AndroidEntryPoint
 class Transaction : Fragment() {
@@ -24,6 +20,11 @@ class Transaction : Fragment() {
     ): View? {
         val binding = FragmentTransactionBinding.inflate(layoutInflater)
 
+
+        return binding.root
+    }
+}
+
 //        db.insertTransaction(com.example.transactionapp.domain.db.model.Transaction(
 //            title = "kotnol",
 //            category = "kotnol",
@@ -32,20 +33,17 @@ class Transaction : Fragment() {
 //            createdAt = Date()
 //        ))
 
-        binding.transactionText.setOnClickListener {
-            db.insertTransaction(com.example.transactionapp.domain.db.model.Transaction(
-                title = "kontol",
-                category = "kontol",
-                nominal = 10000,
-                location = "tolol",
-                createdAt = Date()
-            ))
-            db.dateAll.observe(requireActivity()){
-                for (i in it){
-                    Log.d("Transaction", i.toString())
-                }
-            }
-        }
-        return binding.root
-    }
-}
+//        binding.transactionText.setOnClickListener {
+//            db.insertTransaction(com.example.transactionapp.domain.db.model.Transaction(
+//                title = "kontol",
+//                category = "kontol",
+//                nominal = 10000,
+//                location = "tolol",
+//                createdAt = Date()
+//            ))
+//            db.dateAll.observe(requireActivity()){
+//                for (i in it){
+//                    Log.d("Transaction", i.toString())
+//                }
+//            }
+//        }
