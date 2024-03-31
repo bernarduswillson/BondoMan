4c76deb8f798ca7a624e08731a0739b131a1ae07
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-31 19:12:29 +0700

    style: standardize navbar styling

--- app/src/main/java/com/example/transactionapp/ui/screen/splash/SplashActivity.kt
+++ app/src/main/java/com/example/transactionapp/ui/screen/splash/SplashActivity.kt
@@ -14,7 +14,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
         }, 2500)
     }
