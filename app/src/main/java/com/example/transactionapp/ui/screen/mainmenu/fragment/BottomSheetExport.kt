7bb8f44bb0ac072be9c7391298d7341fa6d8f204
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-04-01 20:53:54 +0700

    feat: bottom sheets styling

--- app/src/main/java/com/example/transactionapp/ui/screen/mainmenu/fragment/BottomSheetExport.kt
+++ app/src/main/java/com/example/transactionapp/ui/screen/mainmenu/fragment/BottomSheetExport.kt
@@ -1,12 +1,15 @@
 package com.example.transactionapp.ui.screen.mainmenu.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.activityViewModels
+import com.example.transactionapp.R
 import com.example.transactionapp.databinding.FragmentBottomSheetBinding
 import com.example.transactionapp.domain.db.model.Transaction
 import com.example.transactionapp.ui.viewmodel.transaction.TransactionViewModel
@@ -17,29 +20,61 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 class BottomSheetExport : BottomSheetDialogFragment() {
     private val db: TransactionViewModel by activityViewModels()
     private lateinit var transactionList: MutableList<Transaction>
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentBottomSheetBinding.inflate(layoutInflater)
+
+        binding.tvSaveTo.text = "Save to local as"
+
         transactionList = mutableListOf()
-        db.transaction.observe(requireActivity()){
+        db.transaction.observe(requireActivity()) {
             transactionList.addAll(it)
         }
 
         binding.xlsxButton.setOnClickListener {
-            saveExcelFile(transactionList, "xlsx", requireContext())
-            Toast.makeText(requireContext(), "File saved to ${requireActivity().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).toString()}", Toast.LENGTH_SHORT).show()
-            dismiss()
+            binding.radioGroup.check(R.id.xlsxButton)
+            binding.xlsxButton.background = resources.getDrawable(R.drawable.rounded_retake_button)
+            binding.xlsxButton.setTextColor(resources.getColor(R.color.G5))
+
+            binding.xlsButton.background = resources.getDrawable(R.drawable.rounded_outline_button)
+            binding.xlsButton.setTextColor(resources.getColor(R.color.N5))
         }
 
         binding.xlsButton.setOnClickListener {
-            saveExcelFile(transactionList, "xls", requireContext())
-            Toast.makeText(requireContext(), "File saved to ${requireActivity().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).toString()}", Toast.LENGTH_SHORT).show()
+            binding.radioGroup.check(R.id.xlsButton)
+            binding.xlsButton.background = resources.getDrawable(R.drawable.rounded_retake_button)
+            binding.xlsButton.setTextColor(resources.getColor(R.color.G5))
+
+            binding.xlsxButton.background = resources.getDrawable(R.drawable.rounded_outline_button)
+            binding.xlsxButton.setTextColor(resources.getColor(R.color.N5))
+        }
+
+        binding.saveButton.setOnClickListener {
+            val selectedFormat = when (binding.radioGroup.checkedRadioButtonId) {
+                R.id.xlsxButton -> "xlsx"
+                R.id.xlsButton -> "xls"
+                else -> null
+            }
+            selectedFormat?.let {
+                saveFile(it)
+            }
             dismiss()
         }
 
         return binding.root
+    }
+
+    private fun saveFile(format: String) {
+        saveExcelFile(transactionList, format, requireContext())
+        Toast.makeText(
+            requireContext(),
+            "File saved to ${requireActivity().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).toString()}",
+            Toast.LENGTH_SHORT
+        ).show()
+        dismiss()
     }
 
     override fun onStart() {
